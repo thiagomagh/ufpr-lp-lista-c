f760c4ea5ab572df82e15de215a3e52cf409e700
Thiago Magalhães <128939957+thiagomagh@users.noreply.github.com>
2025-09-03 23:17:57 -0300

    Add new files

--- variaveis/ex03/programa.c
+++ variaveis/ex03/programa.c
@@ -4,19 +4,7 @@
 /* Faça um programa que receba três notas, calcule e mostre a média aritmética. */
 
 int main() {
-    float nota1, nota2, nota3, media;
-
-    printf("\n");
-    printf("Digite as notas abaixo para descobrir a media.\n");
-    printf("Nota 1: ");
-    scanf("%f", &nota1);
-    printf("Nota 2: ");
-    scanf("%f", &nota2);
-    printf("Nota 3: ");
-    scanf("%f", &nota3);
-
-    media = (nota1 + nota2 + nota3) / 3;
-    printf("Media das notas: %.2f\n", media);
+    printf("Ola, Mundo!\n");
 
     return 0;
 } 
